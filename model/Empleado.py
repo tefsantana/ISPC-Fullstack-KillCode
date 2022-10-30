5dc2ac318a52f6256de97879bf662513f5aa25b3
MDLDeveloper <mariodanielledesma@gmail.com>
2022-10-30 13:26:45 -0300

    ABM empleado - Trabajo en progreso

--- model/Empleado.py
+++ model/Empleado.py
@@ -1,10 +1,21 @@
 import ConectDB
 class Empleado:
     def __init__(self) -> None:
-        self.__conet= ConectDB.Conectar()
+        self.__conet= ConectDB.Conectar().getConet()
 
     def registrarEmpleado(self, obj):
-        cur = self.__conet.conexion.cursor()
-        cur.execute("SHOW DATABASES")
-        for x in cur:
-            print(x)
+        #recibe el objeto empleado a registrar en la base
+        cur = self.__conet.cursor()
+        cur.execute("INSERT INTO empleados VALUES ('')")
+    def eliminarEmpleado(self, id):
+        #recibe el id del empleado a eliminar
+        cur = self.__conet.cursor()
+        cur.execute("DELETE FROM empleados WHERE")
+    def modificarEmpleado(self, id):
+        #recibe el id empleado para buscar y retornar los datos
+        cur = self.__conet.cursor()
+        cur.execute("SELECT * FROM empleados JOIN")
+    def actualizarEmpleado(self, obj):
+        #recibe el objeto empleado a actualizar
+        cur = self.__conet.cursor()
+        cur.execute("UPDATE")
