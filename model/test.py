abe039eecf7f3af7fe378ee27a002ff9f5ad9829
Leonardo Cuestas Manthai <99766205+Leoo10@users.noreply.github.com>
2022-11-02 12:45:10 -0300

    Acá trabajo desde la perspectiva del modelo

---
+++ model/test.py
@@ -0,0 +1,12 @@
+import EmpleadoM
+empleado = EmpleadoM.Empleado()
+empleado.registrarEmpleado()
+"""import mariadb.connector
+
+mydb = mariadb.connector.connect(
+  host="localhost",
+  user="root",
+  password="123456"
+)
+
+print(mydb.execute("SHOW DATABASES"))"""
